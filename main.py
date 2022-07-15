eda3d6dbfd5a0bde9d57b3a7055179a555db2b3b
AbhinabK <abhinabk123@gmail.com>
2022-07-15 22:09:16 +0530

    calculation method  working

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import argparse
+import sys
 from simpleCalc.calc import Calculation 
 import argparse 
 
 
 def main():
     # TODO parse the agument from the commandline stroe in a stack and add the values
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('add_ip',type=str,help='adds input')
+    parser.add_argument("exp",nargs='?', type=str,
+                        help="display the expression ")
+
+    parser.add_argument("-c", "--calc",
+                        help="calculate the expression")
+
     args = parser.parse_args()
-    Calculation.add(args.add_ip)
-    # print(vars(args))
+
+    result = args.exp
+
+    if args.calc:
+        result = Calculation.calculate(args.calc)
+
+    print(result)
+
 
 if __name__ == "__main__":
     main()
+    
--- simpleCalc/calc.py
+++ simpleCalc/calc.py
@@ -1,6 +1,11 @@
 class Calculation:
+    # def add(self,s):
+    #    return 10000
+
     @classmethod
-    def add(self,s):
-        print(s)
+    def calculate(self,s):
+
+        return s
+
 
         
