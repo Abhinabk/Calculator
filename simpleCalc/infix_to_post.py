b30d8c4bc79ed3edf9efd8a1bd9bf0556ec670b7
AbhinabK <abhinabk123@gmail.com>
2022-07-19 22:08:02 +0530

    infixtopostfix finally working

--- simpleCalc/infix_to_post.py
+++ simpleCalc/infix_to_post.py
@@ -1,49 +1,85 @@
+import re 
+
 operatorlookup = set(('+', '-' , '*' , '/' , '^' ))
 precedence = {'+':1, '-':1, '*':2, '/':2, '%':2, '^':3}
+
 def infixToPostfix(exp):
     ''' Given an infix expression returns a postfix expression '''
-    postfix = ""
+    postfix = []
     stack = []
-
-    for i in exp:
+    # whitout this 500 will be seen as 5 0 0 so wrong asnwers were giving 
+    tokens = re.findall(r"(\b\w*[\.]?\w+\b|[\(\)\^\+\*\-\/])", exp)
+    for i in tokens:
         # if an operand
         if i.isalnum():
-            postfix+=i
+            postfix.append(i)
 
         elif  i == '(':
             stack.append(i)
 
         elif i == ')':
-            top = stack.pop()
-            while stack and top!='(':
-                postfix += top
-                top = stack.pop()
+            while stack and stack[-1]!='(':
+                postfix.append(stack.pop())
+            stack.pop()
 
         if i in operatorlookup:
-            while stack and stack[-1]!='(' and precedence[i] <= precedence[stack[-1]]:
-                postfix += stack.pop()
+            while stack and stack[-1]!='(' and (precedence[i] <= precedence[stack[-1]]):
+                postfix.append(stack.pop())
             stack.append(i)
 
         if i not in operatorlookup and i not in ('(',')') and i.isdigit()==False and i.isalpha()==False:
            postfix = f"'{i}' not suported yet"
            return postfix 
-
         
     while stack:
-         postfix += stack.pop()
+         postfix.append(stack.pop())
 
-    return ' '.join(postfix)
+    return " ".join(postfix)
+   
 
+
+def eval_postfix(expression):
+    stack = []
+    exp = infixToPostfix(expression)
+    tokens = exp.split()
+    print(tokens)
+    for i in tokens:
+        if i.isdigit():
+            stack.append(i)  
+        else:
+            a = stack.pop()
+            b = stack.pop()
+         
+            if i=='+':
+                res=int(b)+int(a) # old val <operator> recent value
+            elif i=='-':
+                res=int(b)-int(a)    
+            elif i=='*':
+                res=int(b)*int(a)
+            elif i=='%':
+                res=int(b)%int(a) 
+            elif i=='/':
+                res=int(b)/int(a)
+            elif i=='^':
+                res=int(b)**int(a)
+    
+            stack.append(res) # fina
+
+    return ''.join(map(str,stack))
+                  
+  
 # make test cases 
-expressions = ['4*2+5*(2+1*3/4)/2', '4^2+5*(2+1)/2',  'A*(B+C)/D']
-ans = ['4 2 * 5 2 1 3 * 4 / + * 2 / +',
-    '4 2 ^ 5 2 1 + * 2 / +',
-    'A B C + * D /']
+# expressions = ['4*2+5*(2+1*3/4)/2', '4^2+5*(2+1)/2',  'A*(B+C)/D']
+# ans = ['4 2 * 5 2 1 3 * 4 / + * 2 / +',
+#     '4 2 ^ 5 2 1 + * 2 / +',
+#     'A B C + * D /']
 
-for i in range(len(expressions)):
-    res = infixToPostfix(expressions[i])
-    print(res)
+# for i in range(len(expressions)):
+#     res = eval_postfix(expressions[i])
+#     print(res)
 
+s = "(10*5)-500"
+print(eval_postfix(s))
     
 
 
